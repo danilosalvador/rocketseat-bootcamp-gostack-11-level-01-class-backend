a45f454ef62efef0dceeac863a6a39c9b9f7eb95
Danilo Salvador <danilosalvador@Mac-mini-de-Danilo.local>
2020-05-12 23:30:51 -0300

    chore:Métodos HTTP

--- src/index.js
+++ src/index.js
@@ -2,9 +2,45 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message:'Hello World21'});
+/**
+ * Métodos HTTP
+ * 
+ * GET: Buscar informações do backend
+ * POST: Criar uma informação no backend
+ * PUT: Alterar uma informação no backend (conjunto de informações)
+ * PATCH: Alterar uma informação no backend (uma informação, algo específico)
+ * DELETE: Excluir uma informação no backend
+ */
+app.get('/projects', (request, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+  ]);
 });
+
+app.post('/projects', (request, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+  return response.json([
+    'Projeto 4',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
+
+app.delete('/projects/:id', (request, reponse) => {
+  return response.json([
+    'Projeto 2',
+    'Projeto 3'
+  ]);
+});
+
 
 app.listen(3333, () => {
   console.log('🚀 Backend started!');
