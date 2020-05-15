34afffa1b051a87c6259b36325f069f9a6986044
Danilo Salvador <danilosalvador@Mac-mini-de-Danilo.local>
2020-05-14 22:44:55 -0300

    chore:Aplicação Funcional

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -21,48 +22,57 @@ app.use(express.json());
   * Route Params: Identificar recursos (atualizar/excluir)
   * Request Body: Conteúdo na hora criar ou editar um recurso (JSON)
   */
+
+const projects = [];
+
 app.get('/projects', (request, response) => {
   const { title } = request.query;
 
-  console.log(title);
+  const result = title 
+    ? projects.filter(project => project.title.includes(title))
+    : projects;
 
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-  ]);
+  return response.json(result);
 });
 
 app.post('/projects', (request, response) => {
-
   const { title, owner } = request.body;
+  const project = { id:uuid(), title, owner };
 
-  console.log(title);
-  console.log(owner);
+  projects.push(project);
 
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  return response.json(project);
 });
 
 app.put('/projects/:id', (request, response) => {
   const { id } = request.params;
+  const { title, owner } = request.body;
 
-  console.log(id);
+  const projectIndex = projects.findIndex(project => project.id === id);
 
-  return response.json([
-    'Projeto 4',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found.'});
+  }
+
+  const project = { id, title, owner };
+  projects[projectIndex] = project;
+
+  return response.json(project);
 });
 
 app.delete('/projects/:id', (request, response) => {
-  return response.json([
-    'Projeto 2',
-    'Projeto 3'
-  ]);
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex(p => p.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found.'})
+  }
+
+  projects.splice(projectIndex, 1);
+
+  //É recomendado o status 204 qdo retornar vazio
+  return response.status(204).send();
 });
 
 
